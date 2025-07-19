e42aac9957cd47658592303dba5b016adc873964
Kaushik Saha <kaushiksaha004@gmail.com>
2025-07-20 04:16:46 +0530

    space optimization

--- BloomFilter/bloomFilter.go
+++ BloomFilter/bloomFilter.go
@@ -2,17 +2,19 @@
 package bloomfilter
 
 import (
+	"math"
+
 	"github.com/spaolacci/murmur3"
 )
 
 type BloomFilter struct {
 	rounds   uint32
-	bitArray []bool
+	bitArray []byte
 }
 
 func NewBloomFilter(size int, rounds uint32) BloomFilter {
 	var bloomFilter BloomFilter
-	bloomFilter.bitArray = make([]bool, size)
+	bloomFilter.bitArray = make([]byte, int(math.Ceil(float64(size)/8)))
 	bloomFilter.rounds = rounds
 	return bloomFilter
 }
@@ -20,16 +22,22 @@ func NewBloomFilter(size int, rounds uint32) BloomFilter {
 func (b *BloomFilter) AddKey(key string) {
 	for i := range b.rounds {
 		hash := murmur3.Sum32WithSeed([]byte(key), i)
-		idx := hash % uint32(len(b.bitArray))
-		b.bitArray[idx] = true
+		idx := hash % uint32(len(b.bitArray)*8)
+		byteIdx := int(math.Floor(float64(idx) / 8))
+		bitPos := idx % 8
+		tmp := byte(1 << (8 - bitPos - 1))
+		b.bitArray[byteIdx] |= tmp
 	}
 }
 
 func (b *BloomFilter) LookupKey(key string) bool {
 	for i := range b.rounds {
 		hash := murmur3.Sum32WithSeed([]byte(key), i)
-		idx := hash % uint32(len(b.bitArray))
-		if !b.bitArray[idx] {
+		idx := hash % uint32(len(b.bitArray)*8)
+		byteIdx := int(math.Floor(float64(idx) / 8))
+		bitPos := idx % 8
+		tmp := byte(1 << (8 - bitPos - 1))
+		if (b.bitArray[byteIdx] & tmp) == byte(0) {
 			return false
 		}
 	}
